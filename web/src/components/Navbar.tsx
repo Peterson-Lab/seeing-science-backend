c5ef2779bcd42a5e5bd4ae50c5de472a5bf00302
Matt Wilkinson <mattwilki17@gmail.com>
2020-12-05 20:28:14 -0500

    start chakra ui

---
+++ web/src/components/Navbar.tsx
@@ -0,0 +1,25 @@
+import { Button, Flex, Heading, HStack, Spacer } from '@chakra-ui/react'
+import React from 'react'
+import { NextChakraImage } from './NextChakraImage'
+
+interface NavbarProps {}
+
+const Navbar: React.FC<NavbarProps> = ({}) => {
+  return (
+    <Flex position="sticky" top={0} zIndex={1} bg="green.500">
+      <Flex flex={1} m="auto" align="center">
+        <HStack ml={4}>
+          <NextChakraImage src="/placeholder.png" dimensions={[50, 50]} />
+          <Heading ml={2}>Seeing Science</Heading>
+        </HStack>
+        <Spacer />
+        <HStack mr={4}>
+          <Button>Login</Button>
+          <Button>Register</Button>
+        </HStack>
+      </Flex>
+    </Flex>
+  )
+}
+
+export default Navbar
--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,56 +1,32 @@
-import {
-  Link as ChakraLink,
-  Text,
-  Code,
-  List,
-  ListIcon,
-  ListItem,
-} from '@chakra-ui/react'
-import { CheckCircleIcon, LinkIcon } from '@chakra-ui/icons'
+import { Box, Stack } from '@chakra-ui/react'
+import React from 'react'
+import Layout from '../components/Layout'
 
-import { Hero } from '../components/Hero'
-import { Container } from '../components/Container'
-import { Main } from '../components/Main'
-import { DarkModeSwitch } from '../components/DarkModeSwitch'
-import { CTA } from '../components/CTA'
-import { Footer } from '../components/Footer'
+interface indexProps {}
 
-const Index = () => (
-  <Container height="100vh">
-    <Hero />
-    <Main>
-      <Text>
-        Example repository of <Code>Next.js</Code> + <Code>chakra-ui</Code> +{' '}
-        <Code>typescript</Code>.
-      </Text>
-
-      <List spacing={3} my={0}>
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          <ChakraLink
-            isExternal
-            href="https://chakra-ui.com"
-            flexGrow={1}
-            mr={2}
-          >
-            Chakra UI <LinkIcon />
-          </ChakraLink>
-        </ListItem>
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          <ChakraLink isExternal href="https://nextjs.org" flexGrow={1} mr={2}>
-            Next.js <LinkIcon />
-          </ChakraLink>
-        </ListItem>
-      </List>
-    </Main>
-
-    <DarkModeSwitch />
-    <Footer>
-      <Text>Next ❤️ Chakra</Text>
-    </Footer>
-    <CTA />
-  </Container>
-)
+const Index: React.FC<indexProps> = ({}) => {
+  return (
+    <Layout>
+      <Hero />
+      <Stack
+        direction={['column', 'row']}
+        spacing="24px"
+        m="auto"
+        justify="center"
+        mt={5}
+      >
+        <Box w="40px" h="40px" bg="yellow.200">
+          1
+        </Box>
+        <Box w="40px" h="40px" bg="tomato">
+          2
+        </Box>
+        <Box w="40px" h="40px" bg="pink.100">
+          3
+        </Box>
+      </Stack>
+    </Layout>
+  )
+}
 
 export default Index
