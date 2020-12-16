35a79fc676cc3c172961255ad36baa4dedbe7e98
Matt Wilkinson <mattwilki17@gmail.com>
2020-12-16 18:29:18 -0500

    add react-psych lib

--- web/src/pages/experiment/reactpsych.tsx
+++ web/src/pages/experiment/reactpsych.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Experiment from '../../components/react-psych/Experiment'
 import { mentalFolding } from '../../experiments/react-psych/mentalFolding'
+import { } from "react-psych"
 
 interface ReactPsychProps {}
 
 const reactPsych: React.FC<ReactPsychProps> = ({}) => {
-  return <Experiment questions={mentalFolding} />
+  return (
+    
+  )
 }
 
 export default reactPsych
