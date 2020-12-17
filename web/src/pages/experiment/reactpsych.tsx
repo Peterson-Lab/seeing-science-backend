3613de55d294c9ac7540f895f2e8af7df7a6ba61
Matt Wilkinson <mattwilki17@gmail.com>
2020-12-17 14:02:25 -0500

    building reactpsysch example

--- web/src/pages/experiment/reactpsych.tsx
+++ web/src/pages/experiment/reactpsych.tsx
@@ -1,13 +1,23 @@
 import React from 'react'
 import Experiment from '../../components/react-psych/Experiment'
 import { mentalFolding } from '../../experiments/react-psych/mentalFolding'
-import {   } from "react-psych"
+import { Timeline, ImageQuestion, ImageQuestionProps } from "react-psych"
 
 interface ReactPsychProps {}
 
+const drtFilePath = "react-psych/DRT/question1"
+
+const question1:ImageQuestionProps ={
+  stimulus
+}
+
+
 const reactPsych: React.FC<ReactPsychProps> = ({}) => {
   return (
-    
+    <Timeline>
+      <ImageQuestion stimulus={`${drtFilePath}/stimulus.png`}  />
+    </Timeline>
+  )
   )
 }
 
