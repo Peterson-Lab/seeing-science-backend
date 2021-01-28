31ae8ab0f822c064f7ed55e692ed2d8aeb47775c
Matt Wilkinson <mattwilki17@gmail.com>
2021-01-27 23:11:40 -0500

    add pages to start of test

--- web/src/pages/experiment/reactpsych.tsx
+++ web/src/pages/experiment/reactpsych.tsx
@@ -46,30 +46,37 @@ const ReactPsych: React.FC = () => {
       <Flex align="center" justify="center">
         <Flex shadow="md" align="center" justify="center" my={5}>
           <Timeline onFinish={finish} size="85">
-            <TextScreen buttonText="Start">
+            <TextScreen buttonText="Next">
               <VStack spacing={4} mx={10} mb={5} textAlign="center">
                 <Heading>Diagrammatic Representations Test</Heading>
-                <Text px={60} mb={6}>
+                <Text px={60} mb={6}></Text>
+              </VStack>
+            </TextScreen>
+            <TextScreen buttonText="Begin">
+              <NextChakraImage
+                src="/exp/drt/danny.png"
+                dimensions={[640, 400]}
+              />
+              <VStack px={60} spacing={2} textAlign="center">
+                <Text>
                   This is Danny - he likes to draw. And he likes to draw things
                   exactly how he sees them, so that his drawings match the real
                   things as closely as possible.
                 </Text>
-                <NextChakraImage
-                  src="/exp/drt/danny.png"
-                  dimensions={[640, 400]}
-                />
-                <VStack px={60} spacing={2} textAlign="center">
-                  <Text>
-                    He‘s drawing the house exactly how it looks and his drawing
-                    matches the house perfectly. This is excellent work!
-                  </Text>
-                  <Text>
-                    So, now I am going to show you some more of Danny’s drawings
-                    and I‘ll show you what he tried to draw. Your job in this
-                    game is to tell me which one is his best drawing, where he
-                    did the best job!
-                  </Text>
-                </VStack>
+                <Text>
+                  He‘s drawing the house exactly how it looks and his drawing
+                  matches the house perfectly. This is excellent work!
+                </Text>
+              </VStack>
+            </TextScreen>
+            <TextScreen buttonText="Next">
+              <VStack px={60} spacing={2} textAlign="center">
+                <Text>
+                  So, now I am going to show you some more of Danny’s drawings
+                  and I‘ll show you what he tried to draw. Your job in this game
+                  is to tell me which one is his best drawing, where he did the
+                  best job!
+                </Text>
               </VStack>
             </TextScreen>
             {questionList.map((q, idx) => {
