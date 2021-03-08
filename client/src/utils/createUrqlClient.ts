f16af4f2145e3c1d5fa45bd17d4c33c17a3e9832
Matt Wilkinson <mattwilki17@gmail.com>
2021-03-08 18:52:51 -0500

    fix client url

--- client/src/utils/createUrqlClient.ts
+++ client/src/utils/createUrqlClient.ts
@@ -21,7 +21,7 @@ export const createUrqlClient = (
   }
 
   return {
-    url: 'http://localhost:4000/graphql',
+    url: process.env.NEXT_PUBLIC_API_URL as string,
     fetchOptions: {
       credentials: 'include' as const,
       headers: cookie ? { cookie } : undefined,
