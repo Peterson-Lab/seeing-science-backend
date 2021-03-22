b11486d58dae234583f4a3c7f43ba68449939808
Matt Wilkinson <mattwilki17@gmail.com>
2021-03-22 11:49:15 -0400

    begin add sentry

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -5,9 +5,12 @@ import { AppProps } from 'next/app'
 import React from 'react'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { ReactQueryDevtools } from 'react-query/devtools'
+import { initSentry } from '../utils/sentry'
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   const queryClient = new QueryClient()
+
+  initSentry()
 
   return (
     <QueryClientProvider client={queryClient}>
